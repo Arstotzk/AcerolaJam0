18fdb0e963261a96ad1653bed44dc1e3d37cc828
Arstotzk <50625444+Arstotzk@users.noreply.github.com>
2024-03-03 23:29:36 +0700

    Add AI navigation

--- Assets/Scripts/Enemies/Puppet/Puppet.cs
+++ Assets/Scripts/Enemies/Puppet/Puppet.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using UnityEngine.AI;
 public class Puppet : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-    public bool isMove = false;
     public bool isCanMove = false;
     public float lastmoveTime;
     public float moveAfterTime = 10f;
@@ -16,6 +15,7 @@ public class Puppet : MonoBehaviour
     private Animator animator;
     public float speed = 5;
     private int runNum = 0;
+    private NavMeshAgent agent;
 
     public Collider damageCollider;
     public Collider fearCollider;
@@ -25,22 +25,13 @@ public class Puppet : MonoBehaviour
         lastmoveTime = Time.time;
         Debug.Log("Time:" + lastmoveTime);
         moveTime = moveAfterTime;
-        animator = this.GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isMove)
-        {
-            Vector3 targetPostition = new Vector3(player.transform.position.x,
-                                       transform.position.y,
-                                       player.transform.position.z);
-            transform.LookAt(targetPostition);
-            transform.position += transform.forward * Time.deltaTime * speed;
-            damageCollider.enabled = true;
-            fearCollider.enabled = true;
-        }
 
         if (isCanMove && Time.time > moveTime && lastmoveTime < moveTime)
         {
@@ -50,7 +41,10 @@ public class Puppet : MonoBehaviour
                 moveTime = Time.time + moveAfterTime;
                 lastmoveTime = Time.time;
                 Debug.Log("MoveTime:" + lastmoveTime);
-                isMove = true;
+                agent.destination = player.transform.position;
+                agent.speed = speed;
+                damageCollider.enabled = true;
+                fearCollider.enabled = true;
                 runNum = Random.Range(0,2);
                 animator.SetBool("run" + runNum, true);
             }
@@ -59,7 +53,7 @@ public class Puppet : MonoBehaviour
     public void StopMove()
     {
         animator.SetBool("run" + runNum, false);
-        isMove = false;
+        agent.speed = 0;
         damageCollider.enabled = false;
         fearCollider.enabled = false;
     }
