9e83c59475d18afa9164e0b313ec31978556633b
Arstotzk <50625444+Arstotzk@users.noreply.github.com>
2024-03-09 09:45:07 +0700

    Add house scene

--- Assets/Scripts/Player/PlayerIteraction.cs
+++ Assets/Scripts/Player/PlayerIteraction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerIteraction : MonoBehaviour
 {
@@ -24,6 +25,11 @@ public class PlayerIteraction : MonoBehaviour
                 Debug.Log("Raycast object: " + hit.transform.name + " distance: " + hit.distance);
                 if (hit.distance < iteractDistance)
                 {
+                    var nextScene = hit.transform.gameObject.GetComponent<NextSceneLoad>();
+                    if (nextScene != null)
+                    {
+                        SceneManager.LoadScene(nextScene.nextSceneName);
+                    }
                     //TODO item -> iteractItem; pickableItem
                     var item = hit.transform.gameObject.GetComponent<Item>();
                     if (item != null)
